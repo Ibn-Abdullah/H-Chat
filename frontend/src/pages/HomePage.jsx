68c651c0f92997556a8b18ad388ccb105c49bb96
Hazem Abdallah <hjaballah244@gmail.com>
2025-09-27 12:02:33 +0300

    s

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useChatStore } from "../store/useChatStore";
 
-import SideBar from "../components/Sidebar";
+import SideBar from "../components/SideBar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 import Footer from "../components/Footer";
