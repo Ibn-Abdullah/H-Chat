a2d8de46df79787fc6bbeb00ae92936a82396926
Hazem Abdallah <hjaballah244@gmail.com>
2025-09-27 12:00:57 +0300

    SideBar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useChatStore } from "../store/useChatStore";
 
-import Sidebar from "../components/Sidebar";
+import SideBar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 import Footer from "../components/Footer";
@@ -13,13 +13,13 @@ const HomePage = () => {
 			<div className="flex items-center justify-center pt-20 px-4">
 				<div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
 					<div className="flex h-full rounded-lg overflow-hidden">
-						<Sidebar />
+						<SideBar />
 
 						{!selectedUser ? <NoChatSelected /> : <ChatContainer />}
 					</div>
 				</div>
 			</div>
-			<Footer/>
+			<Footer />
 		</div>
 	);
 };
