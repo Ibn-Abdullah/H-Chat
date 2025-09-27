19c8a38e1f432a357d05f1c8b72c8bc7c52e36af
Hazem Abdallah <hjaballah244@gmail.com>
2025-09-27 13:17:14 +0300

    axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.MODE === "development"
 			? "http://localhost:5001/api"
-			: "/api",
+			: "https://h-chat-production.up.railway.app/api",
 	withCredentials: true,
 });
