236580fd4fca46dfcb9eeadfbfed92260ee94548
Ibn-Abdullah <hjaballah244@gmail.com>
2025-11-01 17:51:51 +0200

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.MODE === "development"
 			? "http://localhost:5001/api"
-			: "https://h-chat-fwrr.onrender.com",
+			: "https://h-chat-fwrr.onrender.com/api",
 	withCredentials: true,
 });
